525d6caf9e236f15ad385f273be1e8471b2abaa3
sophieprofile <161516240+sophieprofile@users.noreply.github.com>
2024-08-14 09:17:03 +0200

    Update Game.java

--- main/Game.java
+++ main/Game.java
@@ -74,7 +74,7 @@ public class Game implements Runnable{
                 case MEET:
                     //Tuple SenderId = sender.getBoardElement(0);
                     //if(p.board.contains(idsender) == true){
-                        for(int i = 0; i < //p.size/len; i++){
+                        for(int i = 0; i < //p.board.length/p.size; i++){
                             String idneeded = p.board[i].get(key); // idk if that's key and if its not better to do just second loop
                             if(idneeded == idsender){
                                 p.MeetsCounter++;
